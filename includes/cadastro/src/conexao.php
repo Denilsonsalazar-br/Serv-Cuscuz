404eaf068749da45c9b70df068d9a70a1f810c90
Denilson Salazar <denilsonsalazarescola@gmail.com>
2024-10-15 15:23:29 -0300

    Update conexao.php
    Apenas um teste no banco de dados

--- includes/cadastro/src/conexao.php
+++ includes/cadastro/src/conexao.php
@@ -2,7 +2,7 @@
 class Conexao {
     public static function getConexao() {
         try {
-            return new PDO("mysql:host=localhost;dbname=serv+cuscuz", "root", "");
+            return new PDO("mysql:host=localhost;dbname=Serv+Cuscuz", "root", "");
         } catch (\PDOException $e) {
             echo 'Erro ao conectar com o banco de dados: ' . $e->getMessage();
             return null;
