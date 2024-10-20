083eceb43872f1c195c0c6cf9e78b24cd4332cb9
Denilson Salazar <denilsonsalazarescola@gmail.com>
2024-10-20 00:17:16 -0300

    Testando o bootstrap

---
+++ Serv+Cuscuz/includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/header.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">  
+    <title>Serv+Cuscuz</title>
+</head>
+<body>   
+<header>
+    <div class="container">
+        <nav class="nav-bar navbar">
+            <div class="logo">
+                <img src="../assets/img/logo-png-reduzida.png" alt="">
+            </div>
+            <div class="nav-list">
+                <ul class="navbar me-auto mb-2 mb-lg-0">
+                    <li class="nav-item1"><a href="#">Home</a></li>
+                    <li class="nav-item"><a href="#">Quem somos?</a></li>
+                    <li class="nav-item"><a href="">item</a></li>
+                </ul>
+            </div>
+            <div class="login">
+                <button href="../view/pages/login.php">login</button>
+            </div>
+        </nav>
+    </div>
+</header>       
+    
--- Serv+Cuscuz/view/pages/home.php
+++ Serv+Cuscuz/view/pages/home.php
@@ -1,3 +1,12 @@
+<?php
+include '../../includes/header.php';
+?>
+<main>
 
 <h1>Pagina principal</h1>
+
 <a href="../../view/pages/logout.php"> Sair</a>
+
+</main>
+<?php 
+include '../../includes/footer.php';
