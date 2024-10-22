b2893d1a9ddf14df7fda8e0868f17f2548746bb0
Denilson Salazar <denilsonsalazarescola@gmail.com>
2024-10-22 15:40:09 -0300

    Alteração Imagem
    o ID estava errado

--- Serv+Cuscuz/model/DAO/src/testeConexaobd.php
+++ Serv+Cuscuz/model/DAO/src/testeConexaobd.php
@@ -3,7 +3,7 @@ require_once '../src/conexaobd.php';
 
 try {
     $conexao = Conexao::getInstance();
-    $stmt = $conexao->query("SELECT * FROM t_perfil");
+    $stmt = $conexao->query("SELECT * FROM t_funcionario");
     $perfil = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     if (count($perfil) > 0) {
