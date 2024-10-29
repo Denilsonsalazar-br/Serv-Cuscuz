dbe7f2c5b56080ef911185f757ebafc2e9ed5816
Denilson Salazar <denilsonsalazarescola@gmail.com>
2024-10-29 15:18:39 -0300

    atuualização 29/10 15:18
    troquei o icone do nome

--- Serv+Cuscuz/pages/verificarLogin.php
+++ Serv+Cuscuz/pages/verificarLogin.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 if (isset($_SESSION['id']) && isset($_SESSION['nome'])) {
-    echo '<img src="../assets/img/usuario.png" alt="Ícone do usuário" class="user-icon">';
+    echo '<img src="../assets/img/usuarioBranco.png" alt="Ícone do usuário" class="user-icon">';
     echo '<span class="user-name">' . htmlspecialchars($_SESSION['nome']) . '</span>';
 } else {
     echo '<a href="../pages/login.php" class="login-link">Login</a>';
