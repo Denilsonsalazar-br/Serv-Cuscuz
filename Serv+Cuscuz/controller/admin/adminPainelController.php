5b8a3080c583a281b08f53c0ff43b61e1d93d190
Denilson Salazar <denilsonsalazarescola@gmail.com>
2024-11-02 17:47:35 -0300

    Atualização 02/11 17:47

--- Serv+Cuscuz/controller/admin/adminPainelController.php
+++ Serv+Cuscuz/controller/admin/adminPainelController.php
@@ -29,11 +29,11 @@ session_start();
     </header>
     <div class="painelAdm">
         <nav >
-            <a href="../../controller/admin/adminPainelController.php">Home</a>
-            <a href="../../view/admin/listaFuncionarios.php">Funcionários</a>
-            <a href="../../view/admin/listaCliente.php">Clientes</a>
+        <a href="../../controller/admin/adminPainelController.php">Home</a>
             <a href="#">Produtos</a>
             <a href="#">Pedidos</a>
+            <a href="../../view/admin/listaFuncionarios.php">Funcionários</a>
+            <a href="../../view/admin/listaCliente.php">Clientes</a>
             <a href="../../view/admin/carrosselHome.php">Carrossel Home</a>
             <a href="#">Relatórios</a>
         </nav>
